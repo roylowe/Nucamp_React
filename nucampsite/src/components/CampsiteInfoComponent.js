1c14a80afeeb63b7972368047632ae589f615ae9
RoyLowe <roylowe96@gmail.com>
2021-08-09 21:53:04 -0400

    add thunk and logger

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -16,6 +16,7 @@ import {
 import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
 import ModalBody from "reactstrap/lib/ModalBody";
+import { Loading} from './LoadingComponent';
 
 const maxLength = (len) => (val) => !val || val.length <= len;
 const minLength = (len) => (val) => val && val.length >= len;
@@ -155,6 +156,28 @@ function RenderComments({ comments, addComment, campsiteId }) {
 }
 
 function CampsiteInfo(props) {
+  if (props.isLoading) {
+    return (
+      <div className="container">
+          <div className="row">
+              <Loading />
+          </div>
+      </div>
+    );
+  }
+  if (props.errMess){
+      return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        </div>
+      );
+  }
+
+
   if (props.campsite) {
     return (
       <div className="container">
